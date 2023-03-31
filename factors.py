8444eaf867734ce59596f9a45b2bc06651af6df2
Karume-lab <danielkarume@outlook.com>
2023-04-01 00:55:53 +0300

    Factorize all the things!

---
+++ factors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys
+
+
+def factorise(num):
+    """factorise a number into two factors
+    Args:
+            num: number to be factorised
+    Returns:
+            fact1: One of the factors
+    """
+    fact1 = 2
+    while num % fact1:
+        fact1 += 1
+    return fact1
+
+
+def main():
+    """Entry point
+    Returns:
+            None
+	"""
+    file_name = sys.argv[1]
+    file = open(file_name, "r")
+    num_list = file.readlines()
+
+    for num in num_list:
+        fact1 = factorise(int(num.rstrip('\n')))
+        fact2 = int(num) // fact1
+        print("{} = {} * {}".format(num.rstrip('\n'), fact1, fact2))
+
+
+if __name__ == "__main__":
+    main()
