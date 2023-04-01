bc537467de5aad6373e6ae45cf17061b55a95ae9
Karume-lab <danielkarume@outlook.com>
2023-04-01 05:20:51 +0300

    RSA Challenge

--- factors.py
+++ factors.py
@@ -1,35 +1,5 @@
 #!/usr/bin/python3
-import sys
+from factors_functions import *
+if __name__ == '__main__':
 
-
-def factorise(num):
-    """Factorise a number into two factors
-    Args:
-            num: number to be factorised
-    Returns:
-            fact1: One of the factors
-    """
-    fact1 = 2
-    while num % fact1:
-        fact1 += 1
-    return fact1
-
-
-def main():
-    """Entry point
-    Returns:
-            None
-    """
-    file_name = sys.argv[1]
-    file = open(file_name, "r")
-    num_list = file.readlines()
-    file.close()
-
-    for num in num_list:
-        fact1 = factorise(int(num.rstrip('\n')))
-        fact2 = int(num) // fact1
-        print("{}={}*{}".format(num.rstrip('\n'), fact2, fact1))
-
-
-if __name__ == "__main__":
-    main()
+    print(unix_time(print_factors))
